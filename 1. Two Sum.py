329a7dcc2dfe1106278e254718075279f402f154
JiwooOh-kr <lala69422@gmail.com>
2023-01-03 21:49:39 +0900

    9. Palindrome Number

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -4,3 +4,17 @@ class Solution(object):
             for j in range(i+1, len(nums)):
                 if nums[i]+nums[j] == target:
                     return [i, j]
+
+'''
+# Solution 1
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}
+        for i, value in enumerate(nums): #1
+            remaining = target - nums[i] #2
+
+            if remaining in seen: #3
+                return [i, seen[remaining]]  #4
+            else:
+                seen[value] = i  #5
+'''
